21551b1f382b1e69abde290166d44d035c5197f4
TobiasNiedermueller <114598146+TobiasNiedermueller@users.noreply.github.com>
2022-11-16 09:11:48 +0100

    Add files via upload
    Commit2

--- Assets/MyGame/Script/Array.cs
+++ Assets/MyGame/Script/Array.cs
@@ -16,7 +16,22 @@ public class Array : MonoBehaviour
         }
 
         arrayColoredImagesRed = GameObject.FindGameObjectsWithTag("RedImage");
-        
+
+
+        //jeweils das erste
+        Debug.Log(array1[0]);
+        Debug.Log(array2[0]);
+        Debug.Log(array3[0]);
+        Debug.Log(array4[0]);
+        Debug.Log(arrayColoredImagesRed[0]);
+
+        //jeweils das letzte
+        Debug.Log(array1[array1.Length - 1]);
+        Debug.Log(array2[array2.Length - 1]);
+        Debug.Log(array3[array3.Length - 1]);
+        Debug.Log(array4[array4.Length - 1]);
+        Debug.Log(arrayColoredImagesRed[arrayColoredImagesRed.Length - 1]);
+
     }
 
   
