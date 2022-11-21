ff853f9b1e6b2079340bd54cff4ef7a872f1a67a
TobiasNiedermueller <114598146+TobiasNiedermueller@users.noreply.github.com>
2022-11-21 15:00:13 +0100

    Add files via upload

--- Assets/MyGame/Script/Array.cs
+++ Assets/MyGame/Script/Array.cs
@@ -1,13 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Array : MonoBehaviour
 {
-    public int[] array;
-    public int[] array1 = new int[4];
-    public int[] array2 = new int[4] { 21, 22, 23, 24 };
-    public int[] array3 = new int[7] {31, 32 ,33, 34, 35, 36, 37};
-    public string[] array4 = new string[10];
-    public GameObject[] arrayColoredImagesRed;
+    [SerializeField]
+    int[] array;
+    [SerializeField]
+    int[] array1 = new int[4];
+    [SerializeField]
+    int[] array2 = new int[4] { 21, 22, 23, 24 };
+    [SerializeField]
+    int[] array3 = new int[7] {31, 32 ,33, 34, 35, 36, 37};
+    [SerializeField]
+    string[] array4 = new string[10];
+    [SerializeField]
+    GameObject[] arrayColoredImagesRed;
+    [SerializeField]
+    int[] arra5 = new int[25];
+    [SerializeField]
+    int[] array6 = new int[250];
+    [SerializeField]
+    int[] array7 = new int[250];
+    [SerializeField]
+    string[] array8 = new string[100];
+    [SerializeField]
+    int[] array9 = new int[1000];
+    [SerializeField]
+    int[] array10 = new int[275];
+    [SerializeField]
+    int[] array11 = new int[1000];
+
+    int a = 20;
+    int b = 0;
+    int c = -1;
+
     void Start()
     {
         for (int i = 0; i < array4.Length; i++)
@@ -39,6 +66,73 @@ public class Array : MonoBehaviour
         }
 
         AlleWerte();
+
+        array1[3] = 3;
+        Debug.Log(array1[3]);
+
+        array2[2] = 203;
+        Debug.Log(array2[2]);
+
+        array3[3] = 333;
+        Debug.Log(array3[3]);
+
+        array4[2] = "heute";
+        Debug.Log(array4[2]);
+
+        arrayColoredImagesRed[2].GetComponent<SpriteRenderer>().color = Color.green;
+
+        for (int i = 0; i < arra5.Length; i ++)
+        {
+            a += 2;
+            arra5[i] = a;
+        }
+
+        for (int i = 0; i < array6.Length; i++)
+        {
+            b += 2;
+            array6[i] = b;
+        }
+
+        for (int i = 0; i < array7.Length; i++)
+        {
+            
+            c += 2;
+            array7[i] = c;
+        }
+
+        for (int i = 1; i < array8.Length; i += 2)
+        {
+            array8[i] = "b";
+        }
+
+        for (int i = 0; i < array8.Length; i += 2)
+        {
+            array8[i] = "a";
+        }
+
+        for (int i = 0; i < array9.Length; i++)
+        {
+            array9[i] = UnityEngine.Random.Range(0, 10);
+            Debug.Log(array9[i]);
+        }
+
+        System.Array.Sort(array9);
+        for (int i = 0; i < array9.Length; i++)
+        {
+            
+            Debug.Log(array9[i]);
+        }
+
+        for (int i = 0; i < array11.Length; i++)
+        {
+            array11[i] = UnityEngine.Random.Range(0, 10);
+        }
+        System.Array.Sort(array11);
+        for (int i = 0; i < array9.Length; i++)
+        {
+            
+            Debug.Log(array9[i]);
+        }
 
     }
 
